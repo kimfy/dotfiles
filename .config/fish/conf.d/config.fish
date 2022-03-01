a6b33590917c1694230c448b21897d3b9dd31756
kimfy <kim@wwan.no>
2022-03-01 18:12:10 +0100

    Added serial numbers for NZXT Kraken Coolers as environment vars. NZXT_G|CPU_SERIAL

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -1,2 +1,5 @@
 set -g theme_color_scheme gruvbox
 alias config='/usr/bin/git --git-dir=/home/kim/.my-dotfiles/ --work-tree=/home/kim'
+
+set NZXT_CPU_SERIAL 6D950F8D525
+set NZXT_GPU_SERIAL 6D71587C525
