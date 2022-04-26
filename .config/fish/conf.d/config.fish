bcb6d13cbcf797a9a2746543c13585e57425ce26
kimfy <kim@wwan.no>
2022-04-26 20:04:02 +0200

    Added liquidctl commands to run to lower fan speed

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -1,5 +1,10 @@
 set -g theme_color_scheme gruvbox
 alias config='/usr/bin/git --git-dir=/home/kim/.my-dotfiles/ --work-tree=/home/kim'
 
+# liquidctl --serial $NZXT_CPU_SERIAL set fan speed 40
+# liquidctl --serial $NZXT_GPU_SERIAL set fan speed 40
+# liquidctl --serial $NZXT_CPU_SERIAL set pump speed 60
+# liquidctl --serial $NZXT_GPU_SERIAL set pump speed 60
+
 set NZXT_CPU_SERIAL 6D950F8D525
 set NZXT_GPU_SERIAL 6D71587C525
