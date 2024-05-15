e1c240e9fb891bb1db149b50503ea8bcc3e79052
Kim Iversen <kim@wwan.no>
2024-05-15 11:25:49 +0200

    feat: descriptions

--- .config/nvim/lua/kimiversen/remap.lua
+++ .config/nvim/lua/kimiversen/remap.lua
@@ -1,28 +1,28 @@
 -- Center file on movement
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "gg","gg0")
-vim.keymap.set("n", "G", "Gzz0")
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Go up half a page and center" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Go down half a page and center" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Go to next search result and center" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Go to previous search result and center" })
+vim.keymap.set("n", "gg","gg0", { desc = "Go to first line and reset cursor to first column" })
+vim.keymap.set("n", "G", "Gzz0", { desc = "Go to last line and reset cursor to first column" })
 
 -- Move selection up/down with J, K. Respects indentation
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc = "Move selection down"})
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc = "Move selection up"})
 
 -- Make current file executable
 vim.keymap.set("n", "<leader>x", function() 
   if os.getenv("OS") == 'Windows_NT' then
     print("chmod +x % is not supported on Windows")
   else
-    vim.nvim_command("<cmd>!chmod +x %<CR>")
+    vim.nvim_cmd("!chmod +x %<CR>") --"<cmd>!chmod +x %<CR>")
   end
 end,
 {silent = true})
 
 -- Tab management
 -- Create a new tab
-  vim.keymap.set("n", "tc", ":tabe<CR>")
+  vim.keymap.set("n", "tc", ":tabe<CR>", { desc = "Create new tab" })
   -- Go to next tab
   vim.keymap.set("n", "tn", "gt")
   -- Go to previous tab
